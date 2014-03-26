8e596c66140e3eecca16b9d2e2e4e62ca517f6e1
flyasuka <flyasuka@sina.com>
2014-03-26 18:13:17 +0800

    Changed to block by controller ID

--- src/cnss/generator/blockctl/BlockCtlQuery.java
+++ src/cnss/generator/blockctl/BlockCtlQuery.java
@@ -17,19 +17,29 @@ public class BlockCtlQuery
    */
   public static void main(String[] args)
   {
-    String walls[] =
+    String[] controllers = new String[32];
+    for (int i = 0; i < 15; i++)
     {
-      "037", "038", "039"
-    };
+      controllers[i] = "PTL1." + String.valueOf(i + 21) + "_W";
+    }
+    for (int i = 0; i < 15; i++)
+    {
+      controllers[i + 15] = "PTL2B." + String.valueOf(i + 21) + "_C";
+    }
+    for (int i = 0; i < 2; i++)
+    {
+      controllers[i + 30] = "PTL2A." + String.valueOf(i + 3);
+    }
+
     StringBuilder sb = new StringBuilder();
     sb.append("update inther_lc.ctl_controller set hostname =")
             .append("'10.0.0.8'")
             .append(" where ");
     int i = 1;
-    for (String wall : walls)
+    for (String controller : controllers)
     {
-      sb.append("id not like ").append("'%").append(wall).append("%'");
-      if (i != walls.length)
+      sb.append("id not like ").append("'").append(controller).append("%'");
+      if (i != controllers.length)
       {
         sb.append(" and ");
       }
@@ -39,6 +49,7 @@ public class BlockCtlQuery
       }
       i++;
     }
+
     System.out.println(sb);
   }
 
